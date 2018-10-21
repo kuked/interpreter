d9efeb31a5b0bbc808eebb3b4342b34dfaf6a4d6
kuked <kedoku@gmail.com>
2018-10-21 10:32:08 +0900

    Add monkey face

--- ruby/intp/lib/intp/repl.rb
+++ ruby/intp/lib/intp/repl.rb
@@ -1,6 +1,21 @@
+require 'pry'
 module Intp
   class Repl
     PROMPT = '>> '
+
+    MONKEY_FACE = <<'FACE'
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ > | < /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+FACE
 
     def start
       env = Environment.new
@@ -11,17 +26,23 @@ module Intp
 
         lexer = Lexer.new(line.chomp)
         parser = Parser.new(lexer)
-        unless parsers.errors.empty?
-          puts parser.errors.length
-          parser.errors.each { |e| puts e }
-          next
-        end
 
         program = parser.parse_program
+        unless parser.errors.empty?
+          print_parse_errors parser.errors
+          next
+        end
 
         evaluated = Evaluator.eval(program, env)
         puts evaluated.inspect if evaluated
       end
+    end
+
+    def print_parse_errors(errors)
+      puts MONKEY_FACE
+      puts 'Woops! We ran into some monkey business here!'
+      puts ' parse errors:'
+      errors.each { |msg| puts "\t#{msg}" }
     end
   end
 end
