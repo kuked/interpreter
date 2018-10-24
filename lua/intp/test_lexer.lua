f4252ac8ef1a46f05f0673d979437905351681fb
kuked <kedoku@gmail.com>
2018-10-24 21:25:53 +0900

    Update test_lexer.lua

--- lua/intp/test_lexer.lua
+++ lua/intp/test_lexer.lua
@@ -12,17 +12,53 @@ _record.new = function(type, literal)
 end
 
 function testNextToken()
-   local input = "=+(){},;"
+   local input = [[
+let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+let result = add(five, ten);
+]]
    
    local tests = {
+      _record.new(token.LET, "let"),
+      _record.new(token.IDENT, "five"),
       _record.new(token.ASSIGN, "="),
-      _record.new(token.PLUS, '+'),
-      _record.new(token.LPAREN, '('),
-      _record.new(token.RPAREN, ')'),
-      _record.new(token.LBRACE, '{'),
-      _record.new(token.RBRACE, '}'),
-      _record.new(token.COMMA, ','),
-      _record.new(token.SEMICOLON, ';'),
+      _record.new(token.INT, "5"),
+      _record.new(token.SEMICOLON, ";"),
+      _record.new(token.LET, "let"),
+      _record.new(token.IDENT, "ten"),
+      _record.new(token.ASSIGN, "="),
+      _record.new(token.INT, "10"),
+      _record.new(token.SEMICOLON, ";"),
+      _record.new(token.LET, "let"),
+      _record.new(token.IDENT, "add"),
+      _record.new(token.ASSIGN, "="),
+      _record.new(token.FUNCTION, "fn"),
+      _record.new(token.LPAREN, "("),
+      _record.new(token.IDENT, "x"),
+      _record.new(token.COMMA, ","),
+      _record.new(token.IDENT, "y"),
+      _record.new(token.RPAREN, ")"),
+      _record.new(token.LBRACE, "{"),
+      _record.new(token.IDENT, "x"),
+      _record.new(token.PLUS, "+"),
+      _record.new(token.IDENT, "y"),
+      _record.new(token.SEMICOLON, ";"),
+      _record.new(token.RBRACE, "}"),
+      _record.new(token.SEMICOLON, ";"),
+      _record.new(token.LET, "let"),
+      _record.new(token.IDENT, "result"),
+      _record.new(token.ASSIGN, "="),
+      _record.new(token.IDENT, "add"),
+      _record.new(token.LPAREN, "("),
+      _record.new(token.IDENT, "five"),
+      _record.new(token.COMMA, ","),
+      _record.new(token.IDENT, "ten"),
+      _record.new(token.RPAREN, ")"),
+      _record.new(token.SEMICOLON, ";"),
       _record.new(token.EOF, '')
    }
    
