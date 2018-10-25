46439e328c4f02fcee000532e6bf83e0ad7a555f
kuked <kedoku@gmail.com>
2018-10-25 20:50:55 +0900

    Fix some misstakes

--- lua/intp/lexer.lua
+++ lua/intp/lexer.lua
@@ -27,7 +27,38 @@ Lexer.new = function(input)
       return tok
    end
 
+   local isLetter = function(ch)
+      return (string.match(ch, "[%l%u_]") ~= nil)
+   end
+
+   local isDigit = function(ch)
+      return (string.match(ch, "%d") ~= nil)
+   end
+
+   local readIdentifier = function()
+      local position = l.position
+      while isLetter(l.ch) do
+         readChar()
+      end
+      return string.sub(l.input, position, l.position - 1)
+   end
+
+   local readNumber = function()
+      local position = l.position
+      while isDigit(l.ch) do
+         readChar()
+      end
+      return string.sub(l.input, position, l.position - 1)
+   end
+
+   local skipWhitespace = function()
+      while string.match(l.ch, "[ \t\n\r]") do
+         readChar()
+      end
+   end
+
    l.nextToken = function()
+      skipWhitespace()
       local tok = newToken(token.EOF, "")
       if     l.ch == "=" then tok = newToken(token.ASSIGN, l.ch)
       elseif l.ch == ";" then tok = newToken(token.SEMICOLON, l.ch)
@@ -37,8 +68,16 @@ Lexer.new = function(input)
       elseif l.ch == "+" then tok = newToken(token.PLUS, l.ch)
       elseif l.ch == "{" then tok = newToken(token.LBRACE, l.ch)
       elseif l.ch == "}" then tok = newToken(token.RBRACE, l.ch)
+      elseif l.ch == ""  then tok = newToken(token.EOF, l.ch)
       else
-         tok = newToken(token.EOF, "")
+         if isLetter(l.ch) then
+            local literal = readIdentifier()
+            return newToken(token.lookupIdent(literal), literal)
+         elseif isDigit(l.ch) then
+            return newToken(token.INT, readNumber())
+         else
+            tok = newToken(token.ILLEGAL, l.ch)
+         end
       end
       readChar()
       return tok
